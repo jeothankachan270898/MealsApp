013e0aec86a4e7508495d5942a0f23b76a070538
jeothankachan270898 <jeothankachan270898@gmail.com>
2022-03-15 17:31:01 +0500

    stack updated

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,15 +1,39 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator, createAppContainer } from '@react-navigation/stack';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
 
-const MealsNavigator = createStackNavigator({
-  Categories: CategoriesScreen,
-  CategoryMeals: {
-    screen: CategoryMealsScreen
-  },
-  MealDetail: MealDetailScreen
-});
+const MealsNavigator = createStackNavigator()
+ 
+function Mystack(){
+  return(
+    <Stack.Navigator>
+      <Stack.Screen
+      name = 'Login'
+      component = {CategoriesScreen}
+      options = {{ headerShown : false}}
+      />
+      <Stack.Screen
+      name = 'Home'
+      component = {CategoryMealsScreen}
+      options = {{ headerShown : false}}
+      />
+      <Stack.Screen
+      name = 'Home'
+      component = {MealDetailScreen}
+      options = {{ headerShown : false}}
+      />
+      
+    </Stack.Navigator>
+  )
+}
 
-export default createAppContainer(MealsNavigator);
+export default function MealsNavigator()
+{
+  return(
+      <NavigationContainer>
+         <Mystack/> 
+      </NavigationContainer>
+  );
+}
